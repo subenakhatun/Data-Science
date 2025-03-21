e97a8f108a6276d13ecc0e908b30ac1a5b571bb6
Subena khatun <subenakhtun@gmail.com>
2025-03-21 10:01:46 +0000

    connect with the link

--- WebScraping/daraz.py
+++ WebScraping/daraz.py
@@ -4,3 +4,5 @@ import time
 from selenium.webdriver.common.by import By
 
 # chrome browser setup
+driver = webdriver.Chrome()
+driver.get('https://www.daraz.com.bd/mens-sneakers/?spm=a2a0e.tm80330838.cate_2_2.1.1add44S844S8T7')
