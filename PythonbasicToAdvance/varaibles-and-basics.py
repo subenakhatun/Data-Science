2d349e92c3925d38e72aa114e760d71759100e99
subenakhatun <subenakhtun@gmail.com>
2026-02-01 09:37:33 +0600

    updated

--- PythonbasicToAdvance/varaibles-and-basics.py
+++ PythonbasicToAdvance/varaibles-and-basics.py
@@ -40,31 +40,31 @@ print(f"Multiply of {a} and {b} = {c}")
 a = input("Enter A Number: ")
 b = input("Enter Another Number: ")
 c = float(a)/float(b)
-print(f"Multiply of {a} and {b} = {c}")
+print(f"Divide of {a} and {b} = {c}")
 
 # 7.Swap two numbers without using a third variable
-a = 6
-b = 90
+a = int(input("Enter a side number: "))
+b = int(input("Enter b side number: "))
 a,b = b,a 
 print(a,b)
 
 # 8.Calculate the area of a rectangle (length & width input)
-a = 6
-b = 90
+a = int(input("Enter a side number: "))
+b = int(input("Enter b side number: "))
 print("Area of a ractangle",a*b)
 
 # 9.Calculate the perimeter of a rectangle (length & width input)
 
 a = int(input("Enter a side number: "))
 b = int(input("Enter b side number: "))
-c = int(input("Enter c side number: "))
+
 
 perimeter = 2*(a+b)
 print(perimeter)
 
 # 10.Convert Celsius to Fahrenheit (user input)  °F = (°C × 9/5) + 32""" 
 
-C = float(input("Enter a Calcius value: "))
+C = float(input("Enter a Celcius value: "))
 F = (C*(9/5))+32
 print(F)
 
