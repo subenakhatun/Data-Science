60bed406e3978f0bc553ce6b094e88973529d473
subenakhatun <subenakhtun@gmail.com>
2026-07-29 23:23:10 +0600

    search algorithm commplete

---
+++ Ai-Driven-Web-Dev/search-algorithm.py
@@ -0,0 +1,14 @@
+# Search algorithm
+contact_list = [
+'salman','nahid','sujon','Latifa','siyam',
+'shuvo','rashed','durjoy','fahim',
+'tanvir','anik','imaran','jahedul',
+'salman','shuvo','durjoy','nahid'
+]
+
+target_list = input("Enter target value: ")
+result_list = []
+for i in range(len(contact_list)):
+    if target_list == contact_list[i]:
+        result_list.append(i)
+print(result_list)
